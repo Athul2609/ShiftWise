bc5f9444cf5ddd34f2ff8e33c8b2841443009eeb
Athul2609 <athul.srinivas.2003@gmail.com>
2025-02-20 20:03:29 +0530

    added allowed_hosts

--- backend/shiftwise_backend/shiftwise_backend/settings.py
+++ backend/shiftwise_backend/shiftwise_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY =  config("SECRET_KEY", cast=str, default=None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["shiftwise-rcfe.onrender.com", "127.0.0.1"]
 
 
 # Application definition
