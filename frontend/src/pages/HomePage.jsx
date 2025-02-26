6552d62cd7c6e640d9ee0c34503fb277cfeb2316
Athul2609 <athul.srinivas.2003@gmail.com>
2025-02-26 20:00:11 +0530

    made algoplan_archive only store previous month details, Home page now shows month of roster, request page shows correct month calendar and also mentions its date

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,31 @@
 // src/pages/Home.jsx
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import RosterTable from '../components/RosterTable';
+import { API_BASE_URL } from "../config";
 
 const HomePage = () => {
+  const [done,setDone] = useState();
+  const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+
+    useEffect(() => {
+      fetch(`${API_BASE_URL}/api/algoplan/archives/`)
+      .then((response) => response.json())
+      .then((data) => setDone(data))
+      .catch((error) => console.error("Error fetching algoplan:", error));
+    }, []);
+
   return (
     <div className="bg-[#7FA1C3] min-h-screen h-full flex flex-col items-center">
-      <h2 className="text-xl font-bold mb-4 text-[#F5EDED]">Doctor Shift Roster</h2>
-      <RosterTable />
+      {
+        done && done.length!=0 &&
+        <>        
+          <h2 className="text-xl font-bold mb-4 text-[#F5EDED]">Doctor Shift Roster for {months[done[0].month-1]}, {done[0].year}</h2>
+          <RosterTable />
+        </>
+      }
     </div>
   );
 };
